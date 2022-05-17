ed080f86f17eeeec2578b7f879c6a146b09ea3d6
hyeonji00 <hgkim5004@naver.com>
2022-05-17 22:16:22 +0900

    GNL: create bonus part

---
+++ get_next_line/get_next_line_bonus.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hyeonjik <hyeonjik@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/17 22:14:48 by hyeonjik          #+#    #+#             */
+/*   Updated: 2022/05/17 22:15:12 by hyeonjik         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <unistd.h>
+# include <fcntl.h>
+# include <stdlib.h>
+
+size_t	ft_strlen(const char *s);
+char	*get_next_line(int fd);
+
+size_t	ft_strlen(const char *s);
+char	*ft_strchr(const char *s, int c);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strdup(const char *s1);
+char	*ft_strjoin(char const *s1, char const *s2);
+
+#endif
