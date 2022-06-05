5a67023a10d3f4287add13cae5b0b1f51fe62321
hyeonji00 <hgkim5004@naver.com>
2022-06-05 16:57:00 +0900

    edit header

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: hyeonjik <hyeonjik@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/16 13:20:39 by hyeonjik          #+#    #+#             */
-/*   Updated: 2022/06/02 15:32:00 by hyeonjik         ###   ########.fr       */
+/*   Updated: 2022/06/05 16:56:24 by hyeonjik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,6 @@
 size_t	ft_strlen(const char *s);
 char	*get_next_line(int fd);
 
-size_t	ft_strlen(const char *s);
 char	*ft_strchr(const char *s, int c);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strdup(const char *s1);
