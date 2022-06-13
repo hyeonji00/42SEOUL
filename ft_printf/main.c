bb774c2b5fb7cdec9b6d586e70a7154154e9a112
hyeonji00 <hgkim5004@naver.com>
2022-06-14 01:00:53 +0900

    edit.. undefined symbols

--- ft_printf/main.c
+++ ft_printf/main.c
@@ -1,29 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hyeonjik <hyeonjik@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/05/24 21:19:42 by hyeonjik          #+#    #+#             */
-/*   Updated: 2022/06/14 00:59:56 by hyeonjik         ###   ########.fr       */
+/*   Created: 2022/06/14 00:46:46 by hyeonjik          #+#    #+#             */
+/*   Updated: 2022/06/14 00:46:57 by hyeonjik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_PRINTF_H
-# define FT_PRINTF_H
+#include "ft_printf.h"
 
-# include <stdlib.h>
-# include <unistd.h>
-# include <stdarg.h>
-# include "./libft/libft.h"
-
-int	ft_printf(const char *str, ...);
-int	ft_printf_c(va_list ap);
-int	ft_printf_s(va_list ap);
-int	ft_printf_dec(va_list ap);
-int	ft_printf_u(va_list ap);
-int	ft_printf_hex(va_list ap, const char c);
-int	ft_printf_p(va_list ap);
-
-#endif
+int	main(void)
+{
+	ft_printf("%d", 10);
+}
